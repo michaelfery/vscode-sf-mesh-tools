c4cfb263dec242f439e01b0b3cfd5d1f0d03243f
Michaël Fery <michael.fery@outlook.com>
2018-09-10 22:15:26 +0200

    add user prompt for template path in deployment profile (#9)

--- src/commands/create-deployment-profile.ts
+++ src/commands/create-deployment-profile.ts
@@ -13,16 +13,49 @@ export async function createDeploymentProfile(templateFile: string, templateUri:
     deployment.SubscriptionId = configuration.get<string>(constants.config.defaultSubscriptionId);
     deployment.ResourceGroup = configuration.get<string>(constants.config.defaultResourceGroup);
     deployment.ResourceGroupLocation = configuration.get<string>(constants.config.defaultResourceGroupLocation);
-    if (templateFile === undefined) {
-        deployment.TemplateFile = configuration.get<string>(constants.config.defaultTemplateFile);
+    if (templateUri === undefined && templateUri === undefined) {
+        // prompt user for template Path Uri (filled with default value)
+        const pick = await vscode.window.showQuickPick(
+            [
+                { label: 'Local File', description: 'Local File', target: 'local' },
+                { label: 'Uri', description: 'Http or Https link', target: 'uri' }
+            ],
+            { placeHolder: 'Select the type of location for the creation template.' });
+        if (pick === undefined) {
+            return;
+        }
+        if (pick.target == "uri") {
+            templateUri = await vscode.window.showInputBox({
+                placeHolder: "Http or Https link of creation template.",
+                value: configuration.get<string>(constants.config.defaultTemplateUri)
+              });
+        }
+        else {
+            // let the user select the creation template file
+            const options: vscode.OpenDialogOptions = {
+                canSelectFolders: false,
+                canSelectFiles: true,
+                canSelectMany: false,
+                openLabel: 'Select',
+                filters: {
+                   'Json files': ['Json'],
+                   'All files': ['*']
+               }
+           };
+           await vscode.window.showOpenDialog(options).then(fileUri => {
+               if (fileUri && fileUri[0]) {
+                   templateFile = fileUri[0].fsPath
+               }
+           });
+           if (templateFile === undefined) {
+               return;
+           }
+        }
     }
-    else {
+    if (templateFile !== undefined) {
         deployment.TemplateFile = templateFile;
     }
-    if (templateUri === undefined) {
-        deployment.TemplateUri = configuration.get<string>(constants.config.defaultTemplateUri);
-    }
-    else {
+    if (templateUri !== undefined) {
         deployment.TemplateUri = templateUri;
     }
     deployment.InlineParameters = configuration.get<JSON>(constants.config.defaultParameters);
@@ -31,4 +64,5 @@ export async function createDeploymentProfile(templateFile: string, templateUri:
     filePath = path.join(<string>vscode.workspace.rootPath, 'deploy', constants.config.deploymentFileName);
     await fileUtils.createFolder(path.join(<string>vscode.workspace.rootPath, 'deploy'));
     await fileUtils.write(filePath, json);
+    await vscode.window.showInformationMessage('Deployment profile was written in ' + filePath);
 }
